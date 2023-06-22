70337ae8516635d5cc74264b511ef73cff707ad8
Rozemberg Darius Andrei <135977849+xdaruis@users.noreply.github.com>
2023-06-22 19:29:41 +0200

    Changed "if else" to a dictionary

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function changeColor() {
-    clearInterval(timerInterval);
-    timerInterval = setInterval(changeColor, 9000);
-    if (document.getElementById("btn").className === "btn btn-danger") {
-        document.getElementById("btn").className = "btn btn-warning";
-    } else if (document.getElementById("btn").className === "btn btn-warning") {
-        document.getElementById("btn").className = "btn btn-success";
-    } else {
-        document.getElementById("btn").className = "btn btn-danger";
-    }
-}
+const dict = {
+  "btn btn-danger": "btn btn-warning",
+  "btn btn-warning": "btn btn-success",
+  "btn btn-success": "btn btn-danger"
+};
 
 let timerInterval = setInterval(changeColor, 9000);
+
+function changeColor() {
+  clearInterval(timerInterval);
+  timerInterval = setInterval(changeColor, 9000);
+  document.getElementById("btn").className = dict[document.getElementById("btn").className];
+}
