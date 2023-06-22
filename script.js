3d7c68b46bf1c30574484436313a443936fd5b36
Rozemberg Darius Andrei <135977849+xdaruis@users.noreply.github.com>
2023-06-22 20:13:15 +0200

    used a dictionary

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const dict = {
-  "btn btn-danger": "btn btn-warning",
-  "btn btn-warning": "btn btn-success",
-  "btn btn-success": "btn btn-danger"
-};
-
 let timerInterval = setInterval(changeColor, 9000);
 
 function changeColor() {
   clearInterval(timerInterval);
   timerInterval = setInterval(changeColor, 9000);
+  const dict = {
+    "btn btn-danger": "btn btn-warning",
+    "btn btn-warning": "btn btn-success",
+    "btn btn-success": "btn btn-danger"
+  };
   document.getElementById("btn").className = dict[document.getElementById("btn").className];
 }
